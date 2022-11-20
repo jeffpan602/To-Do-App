a1782bfd6dde8a80a3d0d66d7cfdce5654586512
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2022-11-20 01:08:00 -0500

    App Header

---
+++ todoapp/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app"><TodoApp/></div>
+</template>
+
+<script>
+import TodoApp from './components/TodoApp';
+
+export default {
+  name: 'App',
+
+  components: {
+    TodoApp,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ todoapp/src/components/TodoApp.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-app style='margin:30px'>
+        <v-card>
+            <!--panel header-->
+            <v-card-title class='headline primary' primary-title style="color: white;">
+                <v-spacer />
+                <span class="fa-solid fa-bars" /> &nbsp; FRAMEWORKS
+                <v-spacer />
+                <v-btn color="primary" elevation="1">
+                    <span class="fa-solid fa-circle-plus" />ADD
+                </v-btn>
+            </v-card-title>
+            <!--panel body-->
+            <v-simple-table style='margin: 20px;'>
+                <template v-slot:default>
+                    <!--table header-->
+                    <thead>
+                        <tr>
+                            <th class="column text-center"> Title </th>
+                            <th class="column text-center"> Description </th>
+                            <th class="column text-center"> Deadline </th>
+                            <th class="column text-center"> Priority </th>
+                            <th class="column text-center"> Is Complete </th>
+                            <th class="column text-center"> Action </th>
+                        </tr>
+                    </thead>
+                    <!--table body-->
+                    <tbody>
+                        <tr>
+                            <td>{{ }}</td>
+                        </tr>
+                    </tbody>
+                </template>
+            </v-simple-table>
+        </v-card>
+    </v-app>
+</template>
+<script>
+//master
+export default {
+    //life cycles
+    created() { },
+    //methods
+    methods: {},
+    //watchers
+    watch: {},
+    //computed
+    computed: {},
+    //global vars
+    data() {
+        return {
+
+        };
+
+    },
+}
+</script>
